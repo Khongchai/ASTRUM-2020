d6d1041da9cd11301af9f91bbe08e3741d421b12
Khongchai <world1955@hotmail.com>
2020-06-03 14:37:36 +0700

    added background in about page and some minor stuff

---
+++ scrollForPlanets.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', () => {
+    const planetObj = document.querySelectorAll(".planetsDecorContainer");
+
+    var scrollObjLength = planetObj.length;
+    var i = 0;
+    for (i = 0; i < scrollObjLength; i++)
+   {
+      var posYshift = window.pageYOffset * planetObj[i].dataset.rate;
+      planetObj[i].style.transform = 'translate3d(0px, -'+posYshift+'px, 0px)';
+      
+    }
+
+  });
